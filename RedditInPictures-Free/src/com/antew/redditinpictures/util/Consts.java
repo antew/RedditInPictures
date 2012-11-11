087e324de0748ae3abb9c8c149065ee6aa3d9365
Antew <antewcode@gmail.com>
2012-11-11 13:36:21 -0500

    Rename Consts class to ConstsFree so that it doesn't match the name of the Consts class in the library.

--- RedditInPictures-Free/src/com/antew/redditinpictures/util/Consts.java
+++
@@ -1,6 +0,0 @@
-package com.antew.redditinpictures.util;
-
-public class Consts extends com.antew.redditinpictures.library.utils.Consts {
-    public static final String ADMOB_ID   = "a14fd4522953241";
-    public static final String REMOVE_ADS = "removeAds";
-}
---
+++ RedditInPictures-Free/src/com/antew/redditinpictures/util/ConstsFree.java
@@ -0,0 +1,11 @@
+package com.antew.redditinpictures.util;
+
+import com.antew.redditinpictures.library.utils.Consts;
+
+public class ConstsFree extends Consts {
+    public static final String ADMOB_ID            = "a14fd4522953241";
+    public static final String DIALOG_UPGRADE      = "Upgrade";
+    public static final String MARKET_INTENT       = "market://details?id=";
+    public static final String REMOVE_ADS          = "removeAds";
+    public static final String PRO_VERSION_PACKAGE = "com.antew.redditinpictures.pro";
+}
