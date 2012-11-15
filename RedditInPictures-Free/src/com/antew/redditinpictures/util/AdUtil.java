1df4e9afb6a5389d03d51089c5c03d1b1b208e0e
Antew <antewcode@gmail.com>
2012-11-15 17:44:18 -0500

    Add test device. Update library POM.

--- RedditInPictures-Free/src/com/antew/redditinpictures/util/AdUtil.java
+++ RedditInPictures-Free/src/com/antew/redditinpictures/util/AdUtil.java
@@ -9,6 +9,8 @@ public class AdUtil {
         adRequest.addTestDevice(AdRequest.TEST_EMULATOR);
         adRequest.addTestDevice("F1B753A6DEEED0887495F26281CCDDA8");
         adRequest.addTestDevice("3F7DDA2355EFDDDC846D539EDEE73AFD");
+        adRequest.addTestDevice("1B09EC5B49C938E43B2E18F10DBBB90B");
+
         return adRequest;
     }
 }
