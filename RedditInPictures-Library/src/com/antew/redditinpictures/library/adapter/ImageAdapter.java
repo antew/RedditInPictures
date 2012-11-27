3145d3a9b75787196912df8b25f0e5054062cef1
Antew <antewcode@gmail.com>
2012-11-26 23:45:10 -0500

    Add javadoc.

--- RedditInPictures-Library/src/com/antew/redditinpictures/library/adapter/ImageAdapter.java
+++ RedditInPictures-Library/src/com/antew/redditinpictures/library/adapter/ImageAdapter.java
@@ -22,14 +22,21 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
-import com.antew.redditinpictures.library.logging.Log;
+import com.antew.redditinpictures.library.imgur.ImgurThumbnailFetcher;
 import com.antew.redditinpictures.library.reddit.RedditApi.PostData;
+import com.antew.redditinpictures.library.ui.ImageGridFragment;
 import com.antew.redditinpictures.library.utils.ImageFetcher;
 
+/**
+ * This is used as the backing adapter for the {@link GridView} in {@link ImageGridFragment}
+ * @author Antew
+ *
+ */
 public class ImageAdapter extends BaseAdapter {
     public static final String    TAG         = "ImageAdapter";
     private final Context         mContext;
@@ -39,6 +46,12 @@ public class ImageAdapter extends BaseAdapter {
     private List<PostData>        mImages;
     private ImageFetcher          mImageFetcher;
 
+    /**
+     * 
+     * @param context The context
+     * @param imageFetcher The image fetcher (currently using a {@link ImgurThumbnailFetcher}
+     * @param urls A list of {@link PostData} objects representing Reddit posts
+     */
     public ImageAdapter(Context context, ImageFetcher imageFetcher, List<PostData> urls) {
         super();
         mContext = context;
@@ -47,10 +60,20 @@ public class ImageAdapter extends BaseAdapter {
         mImageViewLayoutParams = new GridView.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
     }
 
+    /**
+     * 
+     * @param position The position in the adapter to retrieve
+     * @return {@link PostData} object at the input position
+     */
     public PostData getPostData(int position) {
         return mImages.get(position);
     }
 
+    /**
+     * Returns the list of PostData objects
+     * 
+     * @return The list of {@link PostData}
+     */
     public List<PostData> getPostData() {
         return mImages;
     }
@@ -76,30 +99,26 @@ public class ImageAdapter extends BaseAdapter {
         return position;
     }
 
-    @Override
-    public int getViewTypeCount() {
-        return super.getViewTypeCount();
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return super.getItemViewType(position);
-    }
-
+    /**
+     * Add {@link PostData} objects to the Adapter, also calls {@link BaseAdapter#notifyDataSetChanged()}
+     * @param images The list of posts to add to the Adapter
+     */
     public void addItems(List<PostData> images) {
         mImages.addAll(images);
         notifyDataSetChanged();
     }
 
+    /**
+     * Clear the list backing the Adapter
+     */
     public void clear() {
         mImages.clear();
+        notifyDataSetChanged();
     }
 
-    @Override
-    public boolean hasStableIds() {
-        return true;
-    }
-
+    /**
+     * Removes all NSFW images fom the Adapter and then calls {@link BaseAdapter#notifyDataSetChanged()}
+     */
     public void removeNsfwImages() {
         Iterator<PostData> it = mImages.iterator();
         while (it.hasNext()) {
@@ -112,12 +131,10 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup container) {
-        Log.i(TAG, "getView position = " + position);
         ViewHolder viewHolder = null;
         if (convertView == null) {
             // if it's not recycled, instantiate and initialize
             convertView = new ImageView(mContext);
-            Log.i("getView", "NOT Recycling imageView");
             viewHolder = new ViewHolder();
             viewHolder.imageView = (ImageView) convertView;
             viewHolder.imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
@@ -126,7 +143,6 @@ public class ImageAdapter extends BaseAdapter {
         } else {
         }
         if (convertView != null && convertView instanceof ImageView) {
-            Log.i("getView", "Recycling imageView");
             // Otherwise re-use the converted view
             viewHolder = (ViewHolder) convertView.getTag();
         } else {
@@ -164,6 +180,10 @@ public class ImageAdapter extends BaseAdapter {
         notifyDataSetChanged();
     }
 
+    /**
+     * Sets the number of columns, this is currently used in the {@link OnGlobalLayoutListener} in {@link ImageGridFragment}
+     * @param numColumns
+     */
     public void setNumColumns(int numColumns) {
         mNumColumns = numColumns;
     }
