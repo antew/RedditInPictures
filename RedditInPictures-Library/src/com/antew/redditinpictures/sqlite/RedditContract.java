31af7b955df8f63a18a13b2794eddec63dbd87c8
Antew <antewcode@gmail.com>
2012-12-12 23:54:44 -0500

    Add table for Login information (modhash, username, cookie). Add projection for post data needed by GridView

--- RedditInPictures-Library/src/com/antew/redditinpictures/sqlite/RedditContract.java
+++ RedditInPictures-Library/src/com/antew/redditinpictures/sqlite/RedditContract.java
@@ -7,18 +7,33 @@ public class RedditContract {
 
     private RedditContract() {};
 
-    public static final String  CONTENT_AUTHORITY = "com.antew.redditinpictures";
-    public static final int     POSTS             = 100;
-    public static final int     POSTS_ID          = 110;
-    public static final Uri     BASE_CONTENT_URI  = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String CONTENT_AUTHORITY = "com.antew.redditinpictures";
+    public static final int    BASE              = 1;
+    public static final int    REDDIT            = 100;
+    public static final int    REDDIT_ID         = 101;
 
-    private static final String PATH_POSTS        = "posts";
-    private static final String PATH_REDDIT_DATA  = "reddit_data";
+    public static final int    POSTS             = 200;
+    public static final int    POSTS_ID          = 201;
+
+    public static final int    LOGIN             = 300;
+    public static final int    LOGIN_ID          = 301;
+
+    public static final Uri    BASE_CONTENT_URI  = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_POSTS        = "posts";
+    public static final String PATH_REDDIT_DATA  = "reddit_data";
+    public static final String PATH_LOGIN        = "login";
 
     public interface RedditDataColumns {
         String MODHASH = "modhash";
         String AFTER   = "after";
         String BEFORE  = "before";
+    }
+
+    public interface LoginColumns {
+        String USERNAME = "username";
+        String MODHASH  = "modhash";
+        String COOKIE   = "cookie";
     }
 
     public interface PostColumns {
@@ -55,14 +70,28 @@ public class RedditContract {
     }
 
     public static class Posts implements PostColumns, BaseColumns {
-        public static final Uri    CONTENT_URI       = BASE_CONTENT_URI.buildUpon().appendPath(PATH_POSTS).build();
-        public static final String CONTENT_TYPE      = "vnd.android.cursor.dir/vnd.redditinpictures.postdata";
-        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.redditinpictures.postdata";
+        public static final Uri      CONTENT_URI         = BASE_CONTENT_URI.buildUpon().appendPath(PATH_POSTS).build();
+        public static final String   CONTENT_TYPE        = "vnd.android.cursor.dir/vnd.redditinpictures.postdata";
+        public static final String   CONTENT_ITEM_TYPE   = "vnd.android.cursor.item/vnd.redditinpictures.postdata";
 
-        public static final String DEFAULT_SORT      = BaseColumns._ID + " ASC";
+        public static final String   DEFAULT_SORT        = BaseColumns._ID + " ASC";
+
+        public static final String[] GRIDVIEW_PROJECTION = new String[] { _ID, URL, THUMBNAIL };
 
         public static Uri buildPostDataUri(long postNumber) {
             return CONTENT_URI.buildUpon().appendPath(String.valueOf(postNumber)).build();
+        }
+    }
+
+    public static class Login implements LoginColumns, BaseColumns {
+        public static final Uri    CONTENT_URI       = BASE_CONTENT_URI.buildUpon().appendPath(PATH_REDDIT_DATA).build();
+        public static final String CONTENT_TYPE      = "vnd.android.cursor.dir/vnd.redditinpictures.login";
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.redditinpictures.login";
+
+        public static final String DEFAULT_SORT      = BaseColumns._ID + " ASC";
+
+        public static Uri buildPostDataUri(String username) {
+            return CONTENT_URI.buildUpon().appendPath(username).build();
         }
     }
 
--- RedditInPictures-Library/src/com/antew/redditinpictures/sqlite/RedditDatabase.java
+++ RedditInPictures-Library/src/com/antew/redditinpictures/sqlite/RedditDatabase.java
@@ -5,23 +5,33 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
+import com.antew.redditinpictures.sqlite.RedditContract.LoginColumns;
 import com.antew.redditinpictures.sqlite.RedditContract.PostColumns;
 import com.antew.redditinpictures.sqlite.RedditContract.RedditDataColumns;
 
 public class RedditDatabase extends SQLiteOpenHelper {
 
-    private static final int    DATABASE_VERSION      = 1;
-    private static final String DATABASE_NAME         = "redditinpictures.db";
+    private static final int    DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME    = "redditinpictures.db";
 
     public interface Tables {
         String POSTDATA    = "post_data";
         String REDDIT_DATA = "reddit_data";
-        
+        String LOGIN       = "login";
+
         interface CreateTableSql {
             //@formatter:off
+            String LOGIN = "CREATE TABLE " + Tables.LOGIN +
+                    " ("
+                    + BaseColumns._ID       + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + LoginColumns.USERNAME + " TEXT NOT NULL, "
+                    + LoginColumns.COOKIE   + " TEXT NOT NULL, "
+                    + LoginColumns.MODHASH  + " TEXT NOT NULL"
+                    + " );";
+                    
             String POSTDATA = "CREATE TABLE " + Tables.POSTDATA + 
                     " (" 
-                    + BaseColumns._ID                               + " INTEGER PRIMARY KEY AUTOINCREMENT, " 
+                    + BaseColumns._ID                    + " INTEGER PRIMARY KEY AUTOINCREMENT, " 
                     + PostColumns.DOMAIN                 + " TEXT NOT NULL, "
                     + PostColumns.BANNED_BY              + " TEXT, "
                     + PostColumns.SUBREDDIT              + " TEXT NOT NULL, "
@@ -82,7 +92,7 @@ public class RedditDatabase extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + Tables.REDDIT_DATA);
         onCreate(db);
     }
-    
+
     public static void deleteDatabase(Context context) {
         context.deleteDatabase(DATABASE_NAME);
     }
