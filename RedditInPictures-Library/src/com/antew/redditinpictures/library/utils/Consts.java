da74b21e5867d7b9a90f824fb615daf0798908c3
Antew <antewcode@gmail.com>
2013-01-14 23:19:55 -0500

    Add new consts.

--- RedditInPictures-Library/src/com/antew/redditinpictures/library/utils/Consts.java
+++ RedditInPictures-Library/src/com/antew/redditinpictures/library/utils/Consts.java
@@ -22,19 +22,23 @@ public class Consts {
     public static final String EXTRA_AGE                      = "age";
     public static final String EXTRA_CATEGORY                 = "category";
     public static final String EXTRA_ENTRIES                  = "Entries";
+    public static final String EXTRA_ERROR_MESSAGE            = "errorMessage";
     public static final String EXTRA_FILENAME                 = "filenameExtra";
     public static final String EXTRA_IMAGE                    = "image";
     public static final String EXTRA_IMAGE_HASH               = "imageHash";
     public static final String EXTRA_IS_SYSTEM_UI_VISIBLE     = "isSystemUiVisible";
+    public static final String EXTRA_MY_SUBREDDITS            = "mySubreddits";
     public static final String EXTRA_NAV_POSITION             = "navPosition";
     public static final String EXTRA_NEWLY_SELECTED_SUBREDDIT = "newSelectedSubreddit";
     public static final String EXTRA_PERMALINK                = "permalink";
     public static final String EXTRA_REDDIT_API               = "redditApi";
     public static final String EXTRA_REDDIT_URL               = "redditUrl";
     public static final String EXTRA_SCORE                    = "score";
+    public static final String EXTRA_SUCCESS                  = "success";
     public static final String EXTRA_SELECTED_SUBREDDIT       = "selectedSubreddit";
     public static final String EXTRA_SHOW_NSFW_IMAGES         = "showNsfwImages";
     public static final String EXTRA_SHOW_NSFW_IMAGES_CHANGED = "showNsfwImagesChanged";
+    public static final String EXTRA_USERNAME                 = "username";
 
     // Constants for dialog tags
     public static final String DIALOG_GET_FILENAME            = "getFilename";
@@ -42,15 +46,23 @@ public class Consts {
     public static final String DIALOG_LOGOUT                  = "logout";
 
     // Constants for broadcast messages
-    public static final String BROADCAST_DOWNLOAD_IMAGE       = "downloadImage";
+    public static final String BROADCAST_DOWNLOAD_IMAGE       = "download-image";
+    public static final String BROADCAST_ABOUT_SUBREDDIT      = "about-subreddit";
     public static final String BROADCAST_HTTP_FINISHED        = "http-finished";
+    public static final String BROADCAST_LOGIN_COMPLETE       = "login-complete";
+    public static final String BROADCAST_MY_SUBREDDITS        = "my-subreddits";
     public static final String BROADCAST_SUBREDDIT_SELECTED   = "subreddit-selected";
-    public static final String BROADCAST_REMOVE_NSFW_IMAGES   = "removeNsfwImages";
-    public static final String BROADCAST_TOGGLE_FULLSCREEN    = "fullscreenToggle";
+    public static final String BROADCAST_SUBSCRIBE            = "subscribe";
+    public static final String BROADCAST_REMOVE_NSFW_IMAGES   = "remove-nsfw-images";
+    public static final String BROADCAST_TOGGLE_FULLSCREEN    = "fullscreen-toggle";
     public static final String BROADCAST_UPDATE_SCORE         = "update-score";
 
-    public static final int    LOADER_REDDIT                  = 1;
-    public static final int    LOADER_POSTS                   = 2;
+    public static final int    LOADER_REDDIT                  = 10;
+    public static final int    LOADER_POSTS                   = 20;
+    public static final int    LOADER_LOGIN                   = 30;
+
+    public static final String COMPACT_URL                    = "/.compact";
+    public static final String REDDIT_BASE_URL                = "http://www.reddit.com";
 
     public static final int    POSTS_TO_FETCH                 = 50;
 }
