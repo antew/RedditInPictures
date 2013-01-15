ec26d629c96ada712ba821d3840ef3de6e41586a
Antew <antewcode@gmail.com>
2013-01-15 00:13:55 -0500

    Add RequestCode enum for the different requests the RedditService currently supports.

---
+++ RedditInPictures-Library/src/com/antew/redditinpictures/library/service/RequestCode.java
@@ -0,0 +1,15 @@
+package com.antew.redditinpictures.library.service;
+
+public enum RequestCode {
+    LOGIN(100), POSTS(110), LOGOUT(120), VOTE(130), MY_SUBREDDITS(140), SUBSCRIBE(150), ABOUT_SUBREDDIT(160);
+
+    private int requestCode;
+
+    RequestCode(int requestCode) {
+        this.requestCode = requestCode;
+    }
+
+    public int getRequestCode() {
+        return requestCode;
+    }
+}
