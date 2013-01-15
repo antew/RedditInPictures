72c6f40cfaa33d8596b94f1c73cd878d8fffc7a8
Antew <antewcode@gmail.com>
2013-01-14 23:18:22 -0500

    Add generic class for deserialization of Gson

---
+++ RedditInPictures-Library/src/com/antew/redditinpictures/library/json/JsonDeserializer.java
@@ -0,0 +1,36 @@
+package com.antew.redditinpictures.library.json;
+
+import com.antew.redditinpictures.library.gson.VoteAdapter;
+import com.antew.redditinpictures.library.logging.Log;
+import com.antew.redditinpictures.library.reddit.Vote;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+public class JsonDeserializer {
+    public static final String TAG = JsonDeserializer.class.getSimpleName();
+    private static Gson gson;
+    
+    public static <T> T deserialize(String json, Class<T> clazz) {
+        try {
+            return getGson().fromJson(json, clazz);
+        } catch (JsonSyntaxException e) {
+            Log.e(TAG, "deserialize - Error parsing JSON!", e);
+        } catch (IllegalStateException e) {
+            Log.e(TAG, "deserialize - Error parsing JSON!", e);
+        }
+        
+        return null;
+    }
+    
+    public static Gson getGson () {
+        if (gson == null) {
+            GsonBuilder builder = new GsonBuilder();
+            builder.registerTypeAdapter(Vote.class, new VoteAdapter());
+            builder.serializeNulls();
+            gson = builder.create();
+        }
+        
+        return gson;
+    }
+}
