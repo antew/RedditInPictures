d254a4ef91c0ea589c1e754bc69a7c0755535c5b
David Pate <davidtpate@gmail.com>
2014-02-10 20:33:29 -0500

    Make classes use same Shared Prefs value closes #29

--- RedditInPictures-Free/src/com/antew/redditinpictures/preferences/SharedPreferencesHelperFree.java
+++ RedditInPictures-Free/src/com/antew/redditinpictures/preferences/SharedPreferencesHelperFree.java
@@ -3,15 +3,15 @@ package com.antew.redditinpictures.preferences;
 import android.content.Context;
 import android.preference.PreferenceManager;
 
-import com.antew.redditinpictures.library.R;
+import com.antew.redditinpictures.R;
 import com.antew.redditinpictures.library.logging.Log;
 import com.antew.redditinpictures.library.preferences.SharedPreferencesHelper;
 
 public class SharedPreferencesHelperFree extends SharedPreferencesHelper {
 
     public static boolean getDisableAds(Context context) {
-        Log.i("getDisableAds", "" + PreferenceManager.getDefaultSharedPreferences(context).getBoolean(context.getString(R.string.disable_ads), false));
-        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(context.getString(R.string.disable_ads), false);
+        Log.i("getDisableAds", "" + PreferenceManager.getDefaultSharedPreferences(context).getBoolean(context.getString(R.string.pref_disable_ads), false));
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(context.getString(R.string.pref_disable_ads), false);
     }
 
 }
