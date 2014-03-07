4ae5153ce5c1f19eb41e1fb8a0cf0d458feb07b5
David Pate <davidtpate@gmail.com>
2014-03-07 00:48:13 -0500

    Fix Free applicationmodule

--- src/free/java/com/antew/redditinpictures/ApplicationModuleFree.java
+++ src/free/java/com/antew/redditinpictures/ApplicationModuleFree.java
@@ -1,15 +1,10 @@
 package com.antew.redditinpictures;
 
-import com.antew.redditinpictures.library.RedditInPicturesApplication;
 import com.antew.redditinpictures.ui.ImageDetailActivityFree;
 import com.antew.redditinpictures.ui.ImageGridActivityFree;
 import com.antew.redditinpictures.ui.ImageGridFragmentFree;
 import com.antew.redditinpictures.ui.ImageListFragmentFree;
-import com.antew.redditinpictures.library.utils.MainThreadBus;
-import com.squareup.otto.Bus;
 import dagger.Module;
-import dagger.Provides;
-import javax.inject.Singleton;
 
 /**
  * Dagger module for setting up provides statements.
@@ -20,7 +15,6 @@ import javax.inject.Singleton;
         complete = false,
 
         injects = {
-            RedditInPicturesApplication.class,
             ImageGridFragmentFree.class,
             ImageListFragmentFree.class,
             ImageGridActivityFree.class,
@@ -28,10 +22,5 @@ import javax.inject.Singleton;
         }, library = true
     )
 public class ApplicationModuleFree {
-
-    @Singleton
-    @Provides Bus provideOttoBus() {
-        return new MainThreadBus(new Bus());
-    }
 
 }
