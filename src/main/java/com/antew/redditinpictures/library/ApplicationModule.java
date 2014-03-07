da02fcc21f522434398bb4638ab4291a8ebb3176
Antew <antewcode@gmail.com>
2014-03-06 23:10:33 -0500

    Add additional classes for injection.

--- src/main/java/com/antew/redditinpictures/library/ApplicationModule.java
+++ src/main/java/com/antew/redditinpictures/library/ApplicationModule.java
@@ -1,10 +1,16 @@
 package com.antew.redditinpictures.library;
 
+import com.antew.redditinpictures.library.ui.ImageDetailActivity;
+import com.antew.redditinpictures.library.ui.ImageGridActivity;
+import com.antew.redditinpictures.library.ui.ImageGridFragment;
+import com.antew.redditinpictures.library.ui.ImageListFragment;
 import com.antew.redditinpictures.library.utils.MainThreadBus;
 import com.squareup.otto.Bus;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
-import javax.inject.Singleton;
 
 /**
  * Dagger module for setting up provides statements.
@@ -16,8 +22,11 @@ import javax.inject.Singleton;
 
         injects = {
             RedditInPicturesApplication.class,
+            ImageGridFragment.class,
+            ImageListFragment.class,
+            ImageGridActivity.class,
+            ImageDetailActivity.class
         }, library = true
-
     )
 public class ApplicationModule {
 
