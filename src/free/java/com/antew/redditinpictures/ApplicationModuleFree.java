d10030e333014d49ac0bdfb3d3f053ddeb8866e8
David Pate <davidtpate@gmail.com>
2014-04-11 00:07:38 -0400

    Fix errors with Free version.

--- src/free/java/com/antew/redditinpictures/ApplicationModuleFree.java
+++ src/free/java/com/antew/redditinpictures/ApplicationModuleFree.java
@@ -1,6 +1,7 @@
 package com.antew.redditinpictures;
 
 import com.antew.redditinpictures.library.service.RedditService;
+import com.antew.redditinpictures.library.ui.ImageDetailFragment;
 import com.antew.redditinpictures.ui.ImageDetailActivityFree;
 import com.antew.redditinpictures.ui.ImageDetailFragmentFree;
 import com.antew.redditinpictures.ui.ImgurAlbumActivityFree;
@@ -20,8 +21,10 @@ import dagger.Module;
     injects = {
         RedditImageGridFragmentFree.class, RedditImageListFragmentFree.class, RedditFragmentActivityFree.class,
         ImageDetailActivityFree.class, ImageDetailFragmentFree.class, ImgurAlbumActivityFree.class, ImgurAlbumFragmentFree.class,
-        RedditService.GetNewPostsIfNeededTask.class,
+        ImageDetailFragment.class,
+        RedditService.GetNewPostsIfNeededTask.class
     }, library = true)
+// TODO: Figure out why this isn't using ImageDetailFragmentFree, but instead only using ImageDetailFragment.
 public class ApplicationModuleFree {
 
 }
