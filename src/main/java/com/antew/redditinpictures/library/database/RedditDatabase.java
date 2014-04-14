10725b87f4b6e29bb391e5fd8f2e8bf73e52f903
Antew <antewcode@gmail.com>
2014-04-14 19:46:32 -0400

    Clean up SQL formatting.

--- src/main/java/com/antew/redditinpictures/library/database/RedditDatabase.java
+++ src/main/java/com/antew/redditinpictures/library/database/RedditDatabase.java
@@ -46,23 +46,21 @@ public class RedditDatabase extends SQLiteOpenHelper {
         switch (oldVersion) {
             case 1:
                 // Database version 1 didn't have the 'priority' column
-                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS + " ADD COLUMN " + SubredditColumns.PRIORITY + " INTEGER DEFAULT 0");
-                db.execSQL(
-                    "ALTER TABLE " + Tables.SUBREDDITS + " ADD COLUMN " + SubredditColumns.USER_IS_SUBSCRIBER + " INTEGER DEFAULT 0");
-                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS + " ADD COLUMN " + SubredditColumns.DEFAULT_SUBREDDIT + " INTEGER DEFAULT 0");
-                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.RETRIEVED_DATE + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS  + " ADD COLUMN " + SubredditColumns.PRIORITY           + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS  + " ADD COLUMN " + SubredditColumns.USER_IS_SUBSCRIBER + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS  + " ADD COLUMN " + SubredditColumns.DEFAULT_SUBREDDIT  + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.RETRIEVED_DATE    + " INTEGER DEFAULT 0");
                 break;
             case 2:
                 //Database version 2 didn't have the 'userIsSubscriber' or 'isDefaultSubreddit' column
-                db.execSQL(
-                    "ALTER TABLE " + Tables.SUBREDDITS + " ADD COLUMN " + SubredditColumns.USER_IS_SUBSCRIBER + " INTEGER DEFAULT 0");
-                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS + " ADD COLUMN " + SubredditColumns.DEFAULT_SUBREDDIT + " INTEGER DEFAULT 0");
-                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.RETRIEVED_DATE + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS  + " ADD COLUMN " + SubredditColumns.USER_IS_SUBSCRIBER + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS  + " ADD COLUMN " + SubredditColumns.DEFAULT_SUBREDDIT  + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.RETRIEVED_DATE    + " INTEGER DEFAULT 0");
                 break;
             case 3:
                 //Database version 3 didn't have the 'isDefaultSubreddit' column
-                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS + " ADD COLUMN " + SubredditColumns.DEFAULT_SUBREDDIT + " INTEGER DEFAULT 0");
-                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.RETRIEVED_DATE + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.SUBREDDITS  + " ADD COLUMN " + SubredditColumns.DEFAULT_SUBREDDIT + " INTEGER DEFAULT 0");
+                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.RETRIEVED_DATE   + " INTEGER DEFAULT 0");
                 break;
             case 4:
                 //Database version 4 didn't have the 'retrievedDate' column.
@@ -70,8 +68,8 @@ public class RedditDatabase extends SQLiteOpenHelper {
                 break;
             case 5:
                 db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.SUBREDDIT + " TEXT");
-                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.CATEGORY + " TEXT");
-                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.AGE + " TEXT");
+                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.CATEGORY  + " TEXT");
+                db.execSQL("ALTER TABLE " + Tables.REDDIT_DATA + " ADD COLUMN " + RedditDataColumns.AGE       + " TEXT");
                 break;
         }
     }
@@ -85,154 +83,86 @@ public class RedditDatabase extends SQLiteOpenHelper {
         interface CreateTableSql {
             String LOGIN = "CREATE TABLE "
                            + Tables.LOGIN
-                           +
-                           " ("
-                           + BaseColumns._ID
-                           + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                           + LoginColumns.USERNAME
-                           + " TEXT NOT NULL, "
-                           + LoginColumns.COOKIE
-                           + " TEXT, "
-                           + LoginColumns.MODHASH
-                           + " TEXT, "
-                           + LoginColumns.SUCCESS
-                           + " INTEGER, "
-                           + LoginColumns.ERROR_MESSAGE
-                           + " TEXT"
+                           + " ("
+                           + BaseColumns._ID            + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                           + LoginColumns.USERNAME      + " TEXT NOT NULL, "
+                           + LoginColumns.COOKIE        + " TEXT, "
+                           + LoginColumns.MODHASH       + " TEXT, "
+                           + LoginColumns.SUCCESS       + " INTEGER, "
+                           + LoginColumns.ERROR_MESSAGE + " TEXT"
                            + " );";
 
             String SUBREDDITS = "CREATE TABLE "
                                 + Tables.SUBREDDITS
-                                +
-                                " ("
-                                + BaseColumns._ID
-                                + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                                + SubredditColumns.DISPLAY_NAME
-                                + " TEXT, "
-                                + SubredditColumns.HEADER_IMAGE
-                                + " TEXT, "
-                                + SubredditColumns.TITLE
-                                + " TEXT, "
-                                + SubredditColumns.URL
-                                + " TEXT, "
-                                + SubredditColumns.DESCRIPTION
-                                + " TEXT, "
-                                + SubredditColumns.CREATED
-                                + " INTEGER, "
-                                + SubredditColumns.CREATED_UTC
-                                + " INTEGER, "
-                                + SubredditColumns.HEADER_SIZE
-                                + " TEXT, "
-                                + SubredditColumns.OVER_18
-                                + " TEXT, "
-                                + SubredditColumns.SUBSCRIBERS
-                                + " INTEGER, "
-                                + SubredditColumns.ACCOUNTS_ACTIVE
-                                + " TEXT, "
-                                + SubredditColumns.PUBLIC_DESCRIPTION
-                                + " TEXT, "
-                                + SubredditColumns.HEADER_TITLE
-                                + " TEXT, "
-                                + SubredditColumns.SUBREDDIT_ID
-                                + " TEXT, "
-                                + SubredditColumns.NAME
-                                + " TEXT, "
-                                + SubredditColumns.PRIORITY
-                                + " INTEGER DEFAULT 0,"
-                                + SubredditColumns.USER_IS_SUBSCRIBER
-                                + " INTEGER DEFAULT 0,"
-                                + SubredditColumns.DEFAULT_SUBREDDIT
-                                + " INTEGER DEFAULT 0"
+                                + " ("
+                                + BaseColumns._ID                     + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                + SubredditColumns.DISPLAY_NAME       + " TEXT, "
+                                + SubredditColumns.HEADER_IMAGE       + " TEXT, "
+                                + SubredditColumns.TITLE              + " TEXT, "
+                                + SubredditColumns.URL                + " TEXT, "
+                                + SubredditColumns.DESCRIPTION        + " TEXT, "
+                                + SubredditColumns.CREATED            + " INTEGER, "
+                                + SubredditColumns.CREATED_UTC        + " INTEGER, "
+                                + SubredditColumns.HEADER_SIZE        + " TEXT, "
+                                + SubredditColumns.OVER_18            + " TEXT, "
+                                + SubredditColumns.SUBSCRIBERS        + " INTEGER, "
+                                + SubredditColumns.ACCOUNTS_ACTIVE    + " TEXT, "
+                                + SubredditColumns.PUBLIC_DESCRIPTION + " TEXT, "
+                                + SubredditColumns.HEADER_TITLE       + " TEXT, "
+                                + SubredditColumns.SUBREDDIT_ID       + " TEXT, "
+                                + SubredditColumns.NAME               + " TEXT, "
+                                + SubredditColumns.PRIORITY           + " INTEGER DEFAULT 0,"
+                                + SubredditColumns.USER_IS_SUBSCRIBER + " INTEGER DEFAULT 0,"
+                                + SubredditColumns.DEFAULT_SUBREDDIT  + " INTEGER DEFAULT 0"
                                 + " );";
 
             String POSTDATA = "CREATE TABLE "
                               + Tables.POSTDATA
-                              +
-                              " ("
-                              + BaseColumns._ID
-                              + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                              + PostColumns.DOMAIN
-                              + " TEXT NOT NULL, "
-                              + PostColumns.BANNED_BY
-                              + " TEXT, "
-                              + PostColumns.SUBREDDIT
-                              + " TEXT NOT NULL, "
-                              + PostColumns.SELFTEXT_HTML
-                              + " TEXT, "
-                              + PostColumns.SELFTEXT
-                              + " TEXT, "
-                              + PostColumns.VOTE
-                              + " TEXT NOT NULL, "
-                              + PostColumns.SAVED
-                              + " INTEGER, "
-                              + PostColumns.POST_ID
-                              + " TEXT NOT NULL, "
-                              + PostColumns.CLICKED
-                              + " INTEGER, "
-                              + PostColumns.TITLE
-                              + " TEXT, "
-                              + PostColumns.COMMENTS
-                              + " INTEGER, "
-                              + PostColumns.SCORE
-                              + " INTEGER, "
-                              + PostColumns.APPROVED_BY
-                              + " TEXT, "
-                              + PostColumns.OVER_18
-                              + " INTEGER, "
-                              + PostColumns.HIDDEN
-                              + " INTEGER, "
-                              + PostColumns.THUMBNAIL
-                              + " TEXT, "
-                              + PostColumns.SUBREDDIT_ID
-                              + " TEXT NOT NULL, "
-                              + PostColumns.AUTHOR_FLAIR_CSS_CLASS
-                              + " TEXT, "
-                              + PostColumns.DOWNS
-                              + " INTEGER, "
-                              + PostColumns.IS_SELF
-                              + " INTEGER, "
-                              + PostColumns.PERMALINK
-                              + " TEXT NOT NULL, "
-                              + PostColumns.NAME
-                              + " TEXT NOT NULL, "
-                              + PostColumns.CREATED
-                              + " INTEGER, "
-                              + PostColumns.URL
-                              + " TEXT, "
-                              + PostColumns.AUTHOR_FLAIR_TEXT
-                              + " TEXT, "
-                              + PostColumns.AUTHOR
-                              + " TEXT NOT NULL, "
-                              + PostColumns.CREATED_UTC
-                              + " INTEGER, "
-                              + PostColumns.LINK_FLAIR_TEXT
-                              + " TEXT, "
-                              + PostColumns.DECODED_URL
-                              + " TEXT, "
-                              + PostColumns.LOADED_AT
-                              + " INTEGER"
+                              + " ("
+                              + BaseColumns._ID                    + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                              + PostColumns.DOMAIN                 + " TEXT NOT NULL, "
+                              + PostColumns.BANNED_BY              + " TEXT, "
+                              + PostColumns.SUBREDDIT              + " TEXT NOT NULL, "
+                              + PostColumns.SELFTEXT_HTML          + " TEXT, "
+                              + PostColumns.SELFTEXT               + " TEXT, "
+                              + PostColumns.VOTE                   + " TEXT NOT NULL, "
+                              + PostColumns.SAVED                  + " INTEGER, "
+                              + PostColumns.POST_ID                + " TEXT NOT NULL, "
+                              + PostColumns.CLICKED                + " INTEGER, "
+                              + PostColumns.TITLE                  + " TEXT, "
+                              + PostColumns.COMMENTS               + " INTEGER, "
+                              + PostColumns.SCORE                  + " INTEGER, "
+                              + PostColumns.APPROVED_BY            + " TEXT, "
+                              + PostColumns.OVER_18                + " INTEGER, "
+                              + PostColumns.HIDDEN                 + " INTEGER, "
+                              + PostColumns.THUMBNAIL              + " TEXT, "
+                              + PostColumns.SUBREDDIT_ID           + " TEXT NOT NULL, "
+                              + PostColumns.AUTHOR_FLAIR_CSS_CLASS + " TEXT, "
+                              + PostColumns.DOWNS                  + " INTEGER, "
+                              + PostColumns.IS_SELF                + " INTEGER, "
+                              + PostColumns.PERMALINK              + " TEXT NOT NULL, "
+                              + PostColumns.NAME                   + " TEXT NOT NULL, "
+                              + PostColumns.CREATED                + " INTEGER, "
+                              + PostColumns.URL                    + " TEXT, "
+                              + PostColumns.AUTHOR_FLAIR_TEXT      + " TEXT, "
+                              + PostColumns.AUTHOR                 + " TEXT NOT NULL, "
+                              + PostColumns.CREATED_UTC            + " INTEGER, "
+                              + PostColumns.LINK_FLAIR_TEXT        + " TEXT, "
+                              + PostColumns.DECODED_URL            + " TEXT, "
+                              + PostColumns.LOADED_AT              + " INTEGER"
                               + " );";
 
             String REDDIT_DATA = "CREATE TABLE "
                                  + Tables.REDDIT_DATA
-                                 +
-                                 " ("
-                                 + BaseColumns._ID
-                                 + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                                 + RedditDataColumns.MODHASH
-                                 + " TEXT NOT NULL, "
-                                 + RedditDataColumns.AFTER
-                                 + " TEXT, "
-                                 + RedditDataColumns.BEFORE
-                                 + " TEXT, "
-                                 + RedditDataColumns.RETRIEVED_DATE
-                                 + " INTEGER DEFAULT 0, "
-                                 + RedditDataColumns.SUBREDDIT
-                                 + " TEXT, "
-                                 + RedditDataColumns.CATEGORY
-                                 + " TEXT, "
-                                 + RedditDataColumns.AGE
-                                 + " TEXT "
+                                 + " ("
+                                 + BaseColumns._ID                  + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                 + RedditDataColumns.MODHASH        + " TEXT NOT NULL, "
+                                 + RedditDataColumns.AFTER          + " TEXT, "
+                                 + RedditDataColumns.BEFORE         + " TEXT, "
+                                 + RedditDataColumns.RETRIEVED_DATE + " INTEGER DEFAULT 0, "
+                                 + RedditDataColumns.SUBREDDIT      + " TEXT, "
+                                 + RedditDataColumns.CATEGORY       + " TEXT, "
+                                 + RedditDataColumns.AGE            + " TEXT "
                                  + ");";
         }
     }
