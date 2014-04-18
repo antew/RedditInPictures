cf30e4987bbd5f1bef8f42d17243d3e4b26e4e33
Antew <antewcode@gmail.com>
2014-04-17 20:51:21 -0400

    Add Otto to BaseFragment, make @Subscribe methods public to make it work

--- src/main/java/com/antew/redditinpictures/library/ui/base/BaseFragment.java
+++ src/main/java/com/antew/redditinpictures/library/ui/base/BaseFragment.java
@@ -3,10 +3,27 @@ package com.antew.redditinpictures.library.ui.base;
 import android.os.Bundle;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.antew.redditinpictures.library.Injector;
+import com.squareup.otto.Bus;
+import javax.inject.Inject;
 
 public class BaseFragment extends SherlockFragment {
+    @Inject
+    protected Bus mBus;
+
     @Override public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Injector.inject(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBus.register(this);
+    }
+
+    @Override
+    public void onPause() {
+        mBus.unregister(this);
+        super.onPause();
     }
 }
